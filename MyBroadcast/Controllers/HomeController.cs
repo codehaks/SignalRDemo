2fa7dd3cbe5128b64103d68f6fdfdf6f56913c88
Hakim Ghods <ghods.hakim@hotmail.com>
2019-02-03 10:59:37 +0330

    Add project files.

---
+++ MyBroadcast/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using MyBroadcast.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBroadcast.Controllers
+{
+    public class HomeController:Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyBroadcast/Controllers/ProgressController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using MyBroadcast.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBroadcast.Controllers
+{
+    public class ProgressController:Controller
+    {
+        private readonly IHubContext<NotifyHub> _hubContext;
+
+        public ProgressController(IHubContext<NotifyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [Route("api/progress")]
+        public async Task<IActionResult> Get()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                await _hubContext.Clients.All.SendAsync("receiveNotify", i);
+                await Task.Delay(100);
+            }
+            return Ok("Done");
+        }
+    }
+}
